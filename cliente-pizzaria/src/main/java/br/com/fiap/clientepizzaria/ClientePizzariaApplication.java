3357afafe22b20774159c7281163d4a81d09c1be
LuizFFarias <rm99519@fiap.com.br>
2024-02-27 10:15:01 -0300

    iniciando pizzaria

--- cliente-pizzaria/src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
+++ cliente-pizzaria/src/main/java/br/com/fiap/clientepizzaria/ClientePizzariaApplication.java
@@ -3,11 +3,24 @@ package br.com.fiap.clientepizzaria;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@RestController
 @SpringBootApplication
+
 public class ClientePizzariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClientePizzariaApplication.class, args);
 	}
 
+	@GetMapping(value ="/")
+	public String index(){
+		String hello = ---
+		Bom dia 2TDSPG!
+
+		Seja Bem vindos a Benezinho Pizzaria
+
+		---
+
+		return "Bom dia 2TDSPG";
+	}
 }
